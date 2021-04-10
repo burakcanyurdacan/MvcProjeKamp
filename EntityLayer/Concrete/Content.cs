ddd195fadde93679c8399e00147665877b692839
Burak Can Yurdacan <burak.yurdacan@arslanmakina.com>
2021-04-10 18:52:49 +0300

    * Projenin katmanları oluşturuldu.
    * EntityLayer katmanında sınıflar tanımlandı.

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        public int ID { get; set; }
+        public string ContentText { get; set; }
+        public DateTime ContentDate { get; set; }
+        //ContentYazar
+        //ContentBaslik
+    }
+}
