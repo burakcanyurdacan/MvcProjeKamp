b2761f6c01e16fda67634809923ff933618ce352
Burak Can Yurdacan <burak.yurdacan@arslanmakina.com>
2021-04-14 10:50:38 +0300

    14.04.2021 Yenilikler
    * EntityLayet sınıfına Entity Framework kütüphanesi eklendi.
    
    * Tüm sınıflardaki property'lere attribute tanımlaması yapıldı.

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,19 @@ namespace EntityLayer.Concrete
 {
     public class About
     {
+        [Key]
         public int AboutId{ get; set; }
+
+        [StringLength(500)]
         public string Details1 { get; set; }
+
+        [StringLength(1000)]
         public string Details2 { get; set; }
+
+        [StringLength(100)]
         public string AboutImg1 { get; set; }
+
+        [StringLength(100)]
         public string AboutImg2 { get; set; }
     }
 }
--- EntityLayer/Concrete/Content.cs
+++ EntityLayer/Concrete/Content.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace EntityLayer.Concrete
 {
     public class Content
     {
+        [Key]
         public int ID { get; set; }
+
+        [StringLength(1000)]
         public string ContentText { get; set; }
+
         public DateTime ContentDate { get; set; }
 
         public int HeadingId { get; set; }
