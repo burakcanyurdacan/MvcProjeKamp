ca91d6fa285da93df4501536c34c5027e3411184
Burak Can Yurdacan <burakcanyurdacan@outlook.com>
2021-05-23 19:28:08 +0300

    23.05.2021 Yenilikler (44 - 48 ders)
    * Heading için controller oluşturuldu ve ekleme işlemi gerçekleştirildi.
    * AddHeading view'ında dropdownlist kullanılarak kategori ve yazar seçildi. Bu dropdownlist'lere, AddHeading metodundaki viewbag'ler ile veri gönderildi.
    * AdminLayout template'inde ilk 3 menüye sayfalar bağlandı.
    * 3 menünün Index sayfalarında üst kısımdaki sayfa başlığı düzenlendi.

--- MvcProjeKamp/Controllers/HeadingController.cs
+++ MvcProjeKamp/Controllers/HeadingController.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.Contrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +13,42 @@ namespace MvcProjeKamp.Controllers
     public class HeadingController : Controller
     {
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
         public ActionResult Index()
-        {
+        { 
             var headingValues = hm.GetHeadingList();
             return View(headingValues);
+        }
+
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> categoryListItem = (from x in cm.GetCategoryList()
+                                                     select new SelectListItem
+                                                     {
+                                                         Text = x.CategoryName,
+                                                         Value = x.CategoryId.ToString()
+                                                     }).ToList();
+
+            List<SelectListItem> writerListItem = (from y in wm.GetWriterList()
+                                                   select new SelectListItem 
+                                                   {
+                                                       Text = y.WriterName+" "+y.WriterSurname,
+                                                       Value = y.ID.ToString()
+                                                   }).ToList();
+
+            ViewBag.clv = new SelectList(cm.GetCategoryList(), "CategoryId", "CategoryName");
+            ViewBag.wrv = writerListItem;
+            //ViewBag.clv = categoryListItem;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.AddingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.AddHeading(p);
+            return RedirectToAction("Index");
         }
     }
 }
