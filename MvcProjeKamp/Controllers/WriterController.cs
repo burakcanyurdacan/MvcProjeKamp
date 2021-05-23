944d5aac32952a1af5377a91c1f7982e63629042
Burak Can Yurdacan <burakcanyurdacan@outlook.com>
2021-05-23 15:27:12 +0300

    23.05.2021 Yenilikler - Yazar sayfası için 2.commit
    * Yazar ekleme ve güncelleme işlemleri yapıldı.
    * Yazar Index sayfasındaki "profili düzenle" butonuna güncelleme sayfası bağlandı.
    * WriterValidator sınıfı oluşturularak gerekli zorunluluklar eklendi.
    * AdminLayout kısmında bir css yazıldı. Bu css, validasyon mesajlarının renginin kırmızı olması için yapıldı.

--- MvcProjeKamp/Controllers/WriterController.cs
+++ MvcProjeKamp/Controllers/WriterController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLogicLayer.Concrete;
+using BusinessLogicLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +14,60 @@ namespace MvcProjeKamp.Controllers
     public class WriterController : Controller
     {
         WriterManager wm = new WriterManager(new EfWriterDal());
+        WriterValidator validations = new WriterValidator();
         public ActionResult Index()
         {
             var writerList = wm.GetWriterList();
             return View(writerList);
+        }
+
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            ValidationResult result = validations.Validate(p);
+            if (result.IsValid)
+            {
+                wm.AddWriter(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult EditWriter(int id)
+        {
+            var writerValue = wm.GetByWriterID(id);
+            return View(writerValue);
+        }
+
+        [HttpPost]
+        public ActionResult EditWriter(Writer p)
+        {
+            ValidationResult result = validations.Validate(p);
+            if (result.IsValid)
+            {
+                wm.UpdateWriter(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
