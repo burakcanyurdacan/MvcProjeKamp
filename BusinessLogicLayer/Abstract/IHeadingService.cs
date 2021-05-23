5446ccfc03c53aee5fdaab9e2adc236ec3eb0dc0
Burak Can Yurdacan <burakcanyurdacan@outlook.com>
2021-05-23 17:44:07 +0300

    23.05.2021 Yenilikler - Başlıklar tablosu için
    * HeadingService, manager ve controller eklendi.

---
+++ BusinessLogicLayer/Abstract/IHeadingService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Abstract
+{
+    public interface IHeadingService
+    {
+        List<Heading> GetHeadingList();
+        void AddHeading(Heading p);
+        void DeleteHeading(Heading p);
+        void UpdateHeading(Heading p);
+        Writer GetByHeadingID(int id);
+    }
+}
---
+++ MvcProjeKamp/Controllers/HeadingController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKamp.Controllers
+{
+    public class HeadingController : Controller
+    {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        public ActionResult Index()
+        {
+            var headingValues = hm.GetHeadingList();
+            return View(headingValues);
+        }
+    }
+}
