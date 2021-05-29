92dff0cf201b849c32451c2b918c60203d855727
Burak Can Yurdacan <burakcanyurdacan@outlook.com>
2021-05-29 14:58:18 +0300

    28.05.2021 Yenilikler (51 ve 52. ders)
    * Content tablosu için BLL'de interface ve manager sınıfları tanımlandı.
    * UI katmanında ContentController eklendi ve başlık ID bilgisine göre içerik getiren action ile view eklendi.
    * Başlıklar view'ında, ilgili başlığa yazılmış içerikleri gösteren action'a yönlendirme yapıldı.

---
+++ BusinessLogicLayer/Abstract/IContentService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Abstract
+{
+    public interface IContentService
+    {
+        List<Content> GetContentList();
+        List<Content> GetContentListById(int id);
+        void AddContent(Content Content);
+        Content GetByContentID(int id);
+        void DeleteContent(Content Content);
+        void UpdateContent(Content Content);
+    }
+}
---
+++ BusinessLogicLayer/Concrete/ContentManager.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLogicLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Concrete
+{
+    public class ContentManager : IContentService
+    {
+        IContentDAL _contentDal;
+
+        public ContentManager(IContentDAL contentDal)
+        {
+            _contentDal = contentDal;
+        }
+
+        public void AddContent(Content Content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteContent(Content Content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Content GetByContentID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Content> GetContentList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Content> GetContentListById(int id)
+        {
+            return _contentDal.List(x => x.HeadingId == id);
+        }
+
+        public void UpdateContent(Content Content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
